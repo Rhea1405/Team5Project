bcbcf68696ad8de1afb916ad502f095b535e89d4
Rhea1405 <61132240+Rhea1405@users.noreply.github.com>
2020-03-27 20:04:26 +0530

    Add files via upload

--- DBOperations.cs
+++ DBOperations.cs
@@ -26,5 +26,32 @@ namespace Team5Project.Models
 
             return "Thank you for registering with us!You can login after your approval.";
         }
+        public static User_Registration CheckRegistration(string username,string password)
+        {
+            
+            User_Registration u = new User_Registration();
+            var l = from v in v.User_Registration
+                    where v.Username == username && v.Password == password
+                    select v;
+
+            return l.First();
+
+        }
+        public static List<Customer_Profile> GetAll()
+        {
+            var c = from cl in v.Customer_Profile
+                    select cl;
+
+            return c.ToList();
+
+        }
+        public static List<Branch_Admin> GetallB()
+        {
+
+            var b = from bl in v.Branch_Admin
+                    select bl;
+
+            return b.ToList();
+        }
     }
 }
--- LoginController.cs
+++ LoginController.cs
@@ -10,8 +10,56 @@ namespace Team5Project.Controllers
     public class LoginController : Controller
     {
         // GET: Login
+        List<Branch_Admin> blist = null;
+        List<Customer_Profile> clist = null;
         public ActionResult Login()
         {
+            return View();
+        }
+        public ActionResult AdminLogin(string username,string password)
+        {
+            User_Registration l = DBOperations.CheckRegistration(username, password);
+            if (l != null)
+            {
+                Session["username"] = l.Username;
+                if (l.Role_Type == "admin" && l.Status == "Approved")
+                {
+                    return RedirectToAction("SuccessfulAdmin");
+                }
+                else if(l.Role_Type=="Customer" && l.Status=="Approved")
+                {
+                    return RedirectToAction("SuccessfulCustomer");
+                }
+                else if(l.Role_Type=="ba" && l.Status=="Approved")
+                {
+                    return RedirectToAction("SuccessfulBA");
+                }
+                else
+                {
+                    return View("WrongLogin");
+                }
+               
+            }
+            else
+            {
+                return View("WrongLogin");
+            }
+
+        }
+        public ActionResult SuccessfulAdmin()
+        {
+            clist = DBOperations.GetAll();
+            ViewBag.clist = clist;
+
+            blist = DBOperations.GetallB();
+            ViewBag.blist = blist;
+            return View();
+
+        }
+        
+        public ActionResult WrongLogin()
+        {
+
             return View();
         }
         public ActionResult Customer()
@@ -31,6 +79,12 @@ namespace Team5Project.Controllers
 
 
 
+
+        }
+        public ActionResult SuccessfulCustomer()
+        {
+
+            return View();
 
         }
         public ActionResult BranchAdmin()
