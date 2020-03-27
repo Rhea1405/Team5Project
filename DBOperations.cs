6b7f81e865987f53e586da220e6e53c4f9ffee4c
Rhea1405 <61132240+Rhea1405@users.noreply.github.com>
2020-03-27 13:37:13 +0530

    Add files via upload

---
+++ DBOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team5Project.Models
+{
+    public class DBOperations
+    {
+
+
+        static OnlineVehicleBookingEntities1 v = new OnlineVehicleBookingEntities1();
+
+        public static string InsertCustomer(Customer_Profile c)
+        {
+
+            v.Customer_Profile.Add(c);
+            v.SaveChanges();
+
+            return "Thank you for registering with us!You can login after your approval.";
+        }
+        public static string InsertBranchAdmin(Branch_Admin b)
+        {
+            v.Branch_Admin.Add(b);
+            v.SaveChanges();
+
+            return "Thank you for registering with us!You can login after your approval.";
+        }
+    }
+}
---
+++ LoginController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Team5Project.Models;
+
+namespace Team5Project.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Customer()
+        {
+            Customer_Profile c = new Customer_Profile();
+            return View();
+
+        }
+        public ActionResult AddCustomer(Customer_Profile c)
+        {
+
+            c.Status = "Pending";
+            ViewBag.msg = DBOperations.InsertCustomer(c);
+
+
+            return View("Customer");
+
+
+
+
+        }
+        public ActionResult BranchAdmin()
+        {
+            Branch_Admin b = new Branch_Admin();
+            return View();
+        }
+        public ActionResult AddBranchAdmin(Branch_Admin b)
+        {
+
+            b.Status = "Pending";
+            ViewBag.msg = DBOperations.InsertBranchAdmin(b);
+            return View("BranchAdmin");
+        }
+        public ActionResult VehicleSearch()
+        {
+            return View();
+        }
+
+
+
+    }
+}
